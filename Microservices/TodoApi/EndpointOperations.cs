4fae7789da9f1a91c6a1feae66655e8c66fe8399
onuryilmaz90 <onuryilmaz90@outlook.com>
2022-12-18 22:04:43 +0100

    - "EndpointOperations" class is added to encapsulate operation logic and simplify "Program" class.

---
+++ Microservices/TodoApi/EndpointOperations.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApi;
+
+internal static class EndpointOperations
+{
+    internal static async Task<IResult> GetAllTodos(TodoDbContext db)
+    {
+        return TypedResults.Ok(await db.Todos.ToArrayAsync());
+    }
+
+    internal static async Task<IResult> GetCompleteTodos(TodoDbContext db)
+    {
+        return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
+    }
+
+    internal static async Task<IResult> GetTodo(int id, TodoDbContext db)
+    {
+        return await db.Todos.FindAsync(id)
+            is Todo todo
+                ? TypedResults.Ok(todo)
+                : TypedResults.NotFound();
+    }
+
+    internal static async Task<IResult> CreateTodo(Todo todo, TodoDbContext db)
+    {
+        db.Todos.Add(todo);
+        await db.SaveChangesAsync();
+
+        return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+    }
+
+    internal static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDbContext db)
+    {
+        var todo = await db.Todos.FindAsync(id);
+
+        if (todo is null) return TypedResults.NotFound();
+
+        todo.Name = inputTodo.Name;
+        todo.IsComplete = inputTodo.IsComplete;
+
+        await db.SaveChangesAsync();
+
+        return TypedResults.NoContent();
+    }
+
+    internal static async Task<IResult> DeleteTodo(int id, TodoDbContext db)
+    {
+        if (await db.Todos.FindAsync(id) is Todo todo)
+        {
+            db.Todos.Remove(todo);
+            await db.SaveChangesAsync();
+            return TypedResults.Ok(todo);
+        }
+
+        return TypedResults.NotFound();
+    }
+}
--- Microservices/TodoApi/Program.cs
+++ Microservices/TodoApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TodoApi;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDbContext>(opt => opt.UseInMemoryDatabase("TodoList"));
@@ -7,50 +8,12 @@ var app = builder.Build();
 
 var todoItems = app.MapGroup("/todoitems");
 
-todoItems.MapGet("/", async (TodoDbContext db) =>
-    await db.Todos.ToListAsync());
+todoItems.MapGet("/", EndpointOperations.GetAllTodos);
 
-app.MapGet("/complete", async (TodoDbContext db) =>
-    await db.Todos.Where(t => t.IsComplete).ToListAsync());
-
-app.MapGet("/{id}", async (int id, TodoDbContext db) =>
-    await db.Todos.FindAsync(id)
-        is Todo todo
-            ? Results.Ok(todo)
-            : Results.NotFound());
-
-app.MapPost("/", async (Todo todo, TodoDbContext db) =>
-{
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/{todo.Id}", todo);
-});
-
-app.MapPut("/{id}", async (int id, Todo inputTodo, TodoDbContext db) =>
-{
-    var todo = await db.Todos.FindAsync(id);
-
-    if (todo is null) return Results.NotFound();
-
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-app.MapDelete("/{id}", async (int id, TodoDbContext db) =>
-{
-    if (await db.Todos.FindAsync(id) is Todo todo)
-    {
-        db.Todos.Remove(todo);
-        await db.SaveChangesAsync();
-        return Results.Ok(todo);
-    }
-
-    return Results.NotFound();
-});
+app.MapGet("/complete", EndpointOperations.GetCompleteTodos);
+app.MapGet("/{id}", EndpointOperations.GetTodo);
+app.MapPost("/", EndpointOperations.CreateTodo);
+app.MapPut("/{id}", EndpointOperations.UpdateTodo);
+app.MapDelete("/{id}", EndpointOperations.DeleteTodo);
 
 app.Run();
