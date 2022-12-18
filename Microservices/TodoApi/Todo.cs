4e34ab6a3e61aa55b734e0cf52e8d3cdbb8013d6
onuryilmaz90 <onuryilmaz90@outlook.com>
2022-12-18 21:47:24 +0100

    - Entity and database context classes are added.

---
+++ Microservices/TodoApi/Todo.cs
@@ -0,0 +1,6 @@
+﻿sealed class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
---
+++ Microservices/TodoApi/TodoDbContext.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+sealed class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
